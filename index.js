e35a031ca99c416006e7b2a72459a1f64fe02d81
Brian Gorman <briangorman99@gmail.com>
2023-07-27 01:58:56 -0400

    refactor svg class

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import fs from "fs";
-import { Circle, Triangle } from "./lib/shapes.js";
+import CLI from "./lib/cli";
 
-let circle = new Circle("SVG", "red", "white").svg;
-
-let triangle = new Triangle("SVG", "white", "red").svg;
-
-fs.writeFile("test.svg", triangle, () => {});
+new CLI().run();
